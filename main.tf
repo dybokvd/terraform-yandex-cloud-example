216472374ea300db18120b1efae4716f604e2fe1
Vladislav Dybok <eden91hazard@gmail.com>
2026-06-10 00:18:26 +0300

    Добавил в конфигурацию Terraform описание необходимой инфраструктуры

--- main.tf
+++ main.tf
@@ -14,3 +14,34 @@ terraform {
 provider "yandex" {
   zone = "ru-central1-a"
 }
+
+# Тип блока, тип ресурса, имя блока
+resource "yandex_compute_instance" "vm-1" {
+  name = "test-vm-terraform"
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  scheduling_policy {
+    preemptible = true
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd806u1okplml22f4pmo"
+    }
+  }
+
+  network_interface {
+    subnet_id = "e9b17c6fjq9vaoq182t9"
+    # Включаем присвоение публичного IP-адреса
+    nat = false
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+    
+  allow_stopping_for_update = true
+} 
